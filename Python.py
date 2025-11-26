b1c8931066b04a1529ece054a7db90c8d63c13f9
Kayky Gazola <kayky@edu.unifil.br>
2025-11-26 19:53:38 -0300

    Implementando Soma

--- Python.py
+++ Python.py
@@ -1,1 +1,9 @@
 #Vamos adiionar aqui as contas
+ 
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = num1 + num2
+
+print("A soma é:", resultado)
